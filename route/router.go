1110fd3201d179b2ab15235e18869ccdab02bd0b
unSerori <pd.ai.374005475@gmail.com>
2024-06-10 07:38:12 +0000

    [ref]ルーティングだけ関数に切り出した

--- route/router.go
+++ route/router.go
@@ -2,25 +2,25 @@ package route
 
 import (
 	"juninry-api/controller"
+	"juninry-api/logging"
 	"juninry-api/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
-func GetRouter() (*gin.Engine, error) {
-	engine := gin.Default() // エンジンを作成
-
+func routing(engine *gin.Engine) *gin.Engine {
 	// endpoints
 	// root page
 	engine.GET("/", controller.ShowRootPage)
 	// json test
+	engine.GET("/test/json", controller.TestJson) // /test
 
 	// endpoints group
 	// ver1グループ
 	v1 := engine.Group("/v1")
 	{
 		// /v1/test
-		v1.GET("test", controller.TestJson)
+		v1.GET("/test/cfmreq", controller.CfmReq)
 
 		// usersグループ
 		users := v1.Group("/users")
@@ -55,10 +55,23 @@ func GetRouter() (*gin.Engine, error) {
 					// /v1/auth/users/notice/{notice_uuid}
 					notice.GET("/:notice_uuid", controller.TestJson) // おしらせ詳細をとる // コントローラで取り出すときは noticeUuid := c.Param("notice_uuid")
 				}
-
 			}
 		}
 	}
+
+}
+
+// エンジンを作成して返す
+func SetupRouter() (*gin.Engine, error) {
+	engine := gin.Default() // エンジンを作成
+
+	routing()
+
+	// テンプレートと静的ファイルを読み込む
+	engine.LoadHTMLGlob("view/views/*.html")
+	engine.Static("/styles", "./view/views/styles") // クライアントがアクセスするURL, サーバ上のパス
+	engine.Static("/scripts", "./view/views/scripts")
+	logging.SuccessLog("Routing completed, start the server.")
 
 	return engine, nil // router設定されたengineを返す。
 }
