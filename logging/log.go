4b2603bb5bc131124f6ae79580c7d16eea18dc29
unSerori <pd.ai.374005475@gmail.com>
2024-06-10 22:54:56 +0000

    [ref]logging初期化処理をinit.goに分けた

--- logging/log.go
+++ logging/log.go
@@ -1,35 +1,9 @@
 package logging
 
 import (
-	"fmt"
-	"io"
 	"log"
-	"os"
 	"time"
 )
-
-var logFile *os.File // ログファイル
-
-// ログファイル出力のセットアップ
-func SetupLogging() error {
-	// ログファイルを作成
-	var err error
-	logFile, err = os.OpenFile("./logging/server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil { // エラーチェック
-		return fmt.Errorf("error opening file: %v", err) // エラーの場合
-	}
-
-	// ログの出力先をファイルにも。
-	log.SetOutput(io.MultiWriter(os.Stdout, logFile))
-	log.Printf("Set up logging.\n\n")
-
-	return nil // ファイルを返す
-}
-
-// ログ初期設定側後にリソースを開放するために実態を返す
-func LogFile() *os.File {
-	return logFile // ログファイルを返す
-}
 
 // 成功時のログをログファイルに残す
 func SuccessLog(successName string) {
