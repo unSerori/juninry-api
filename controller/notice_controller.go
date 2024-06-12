5e4506893b24531aa854a4490de0968d780c691f
MSI\2230294 <2230294@ecc.ac.jp>
2024-06-12 06:46:15 +0000

    [update]お知らせ一覧取得の不要なコードを削除

--- controller/notice_controller.go
+++ controller/notice_controller.go
@@ -1,50 +1,13 @@
 package controller
 
 import (
+	"github.com/gin-gonic/gin"
 	"juninry-api/logging"
 	"juninry-api/service"
 	"net/http"
-	"github.com/gin-gonic/gin"
 )
 
 var noticeService = service.NoticeService{} // サービスの実体を作る。
-
-func GetAllNoticesHandler(ctx *gin.Context) {
-	// 絞り込み条件
-	userUuid := "3cac1684-c1e0-47ae-92fd-6d7959759224"
-	// userUuidからお知らせ一覧を持って来る(厳密にはserviceにuserUuidを渡す)
-	notices, err := noticeService.FindAllNotices(userUuid)
-	// 取得できなかった時のエラーを判断
-	if err != nil {
-		// エラーログ
-		logging.ErrorLog("notice find error", err)
-		// レスポンス(StatusInternalServerError サーバーエラー500番)
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"srvResData": gin.H{},
-		})
-		return //　<-返すよって型指定してないから切り上げるだけ
-	}
-
-	//確認用
-	//log.Println(get_notices)
-
-	// 取得してきたnotice(構造体)を整形する(noticeTitleだけの配列を作る)
-	var noticeTitles []string
-	for _, notice := range notices {
-		title := notice.NoticeTitle
-		//　append(データを入れる配列, 入れたいデータ)
-		noticeTitles = append(noticeTitles, title)
-	}
-
-	// レスポンス(StatusOK　成功200番)
-	ctx.JSON(http.StatusOK, gin.H{
-		"srvResData": gin.H{
-			"notices": noticeTitles,
-		},
-	})
-}
-
-// 関数名の先頭が大文字の場合、pubulic
 
 // お知らせ1件取得
 func GetNoticeDetailHandler(ctx *gin.Context) {
@@ -63,9 +26,12 @@ func GetNoticeDetailHandler(ctx *gin.Context) {
 		return
 	}
 
+	// 成功ログ
+	logging.SuccessLog("Successful noticeDetail get.")
 	// レスポンス(StatusOK　成功200番)
 	ctx.JSON(http.StatusOK, gin.H{
-		"srvResData":  noticeDetail,
+		"srvResMsg":  "Successful noticeDetail get.",
+		"srvResData": noticeDetail,
 	})
 
 }
--- service/notice_service.go
+++ service/notice_service.go
@@ -8,34 +8,6 @@ import (
 
 type NoticeService struct{} // コントローラ側からサービスを実体として使うため。この構造体にレシーバ機能でメソッドを紐づける。
 
-
-
-// userUuidからお知らせ一覧を持って来る
-func (s *NoticeService) FindAllNotices(userUuid string) ([]model.Notice, error) {
-
-	// userUuidを条件にしてclassUuidを取ってくる
-	// 1 - userUuidからclass_menbershipの構造体を取ってくる
-	classMemberships, err := model.FindClassMemberships(userUuid)
-	if err != nil {
-		return nil, err
-	}
-
-	// 2 - 構造体からclassUuidのスライス(配列)を作る
-	var classUuids []string
-	for _, classMembership := range classMemberships {
-		classUuid := classMembership.ClassUuid
-		classUuids = append(classUuids, classUuid)
-	}
-
-	// classUuidを条件にしてnoticeの構造体を取ってくる
-	notices, err := model.FindNotices(classUuids)
-	if err != nil {
-		return nil, err
-	}
-
-	return notices, nil
-}
-
 // おしらせテーブル
 type NoticeDetail struct { // typeで型の定義, structは構造体
 	NoticeTitle       string	//お知らせのタイトル
@@ -45,6 +17,7 @@ type NoticeDetail struct { // typeで型の定義, structは構造体
 	ClassName         string	// どのクラスのお知らせか
 }
 
+//お知らせ詳細取得
 func (s *NoticeService) GetNoticeDetail(noticeUuid string) (NoticeDetail, error) {
 
 	//お知らせ詳細情報取得
