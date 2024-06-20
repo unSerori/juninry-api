6a961d9fbcb6647c94a8fc11eb62111186bdf77d
MSI\2230294 <2230294@ecc.ac.jp>
2024-06-20 03:08:32 +0000

    [update]お知らせの新規登録の処理追加

--- controller/notice_controller.go
+++ controller/notice_controller.go
@@ -1,14 +1,122 @@
 package controller
 
 import (
+	"errors"
+	"fmt"
+	"juninry-api/common"
 	"juninry-api/logging"
+	"juninry-api/model"
 	"juninry-api/service"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
 )
 
 var noticeService = service.NoticeService{} // サービスの実体を作る。
+
+// 新規お知らせ登録
+func RegisterNoticeHandler(ctx *gin.Context) {
+
+	// 構造体にマッピング
+	var bNotice model.Notice // 構造体のインスタンス
+	if err := ctx.ShouldBindJSON(&bNotice); err != nil {
+		// エラーログ
+		logging.ErrorLog("Failure to bind request.", err)
+		// レスポンス
+		resStatusCode := http.StatusBadRequest
+		ctx.JSON(resStatusCode, gin.H{
+			"srvResMsg":  http.StatusText(resStatusCode),
+			"srvResData": gin.H{},
+		})
+		return
+	}
+
+	// // ユーザーを特定する
+	id, exists := ctx.Get("id")
+	if !exists { // idがcに保存されていない。
+		// エラーログ
+		logging.ErrorLog("The id is not stored.", nil)
+		// レスポンス
+		resStatusCode := http.StatusInternalServerError
+		ctx.JSON(resStatusCode, gin.H{
+			"srvResMsg":  http.StatusText(resStatusCode),
+			"srvResData": gin.H{},
+		})
+		return
+	}
+	idAdjusted := id.(string) // アサーション
+	fmt.Println(idAdjusted)		//　アサーションの確認
+
+	// 登録処理と失敗レスポンス
+	token, err := noticeService.RegisterNotice(bNotice)
+	if err != nil { // エラーハンドル
+		// 処理で発生したエラーのうちDB関連のエラーのみ
+		var mysqlErr *mysql.MySQLError // DBエラーを判定するためのDBインスタンス
+		if errors.As(err, &mysqlErr) { // 第一引数のerrが第二引数の型にキャスト可能ならキャストしてtrue
+			// 本処理時のエラーごとに処理(:DBエラー)
+			switch err.(*mysql.MySQLError).Number {
+			case 1062: // 一意性制約違反
+				// エラーログ(同じお知らせが存在してる)
+				logging.ErrorLog("There is already a notice with the same primary key. Uniqueness constraint violation.", err)
+				// レスポンス
+				resStatusCode := http.StatusBadRequest
+				ctx.JSON(resStatusCode, gin.H{
+					"srvResMsg":  http.StatusText(resStatusCode),
+					"srvResData": gin.H{},
+				})
+			default:
+				// エラーログ
+				logging.ErrorLog("New user registration was not possible due to other DB problems.", err)
+				// レスポンス
+				resStatusCode := http.StatusBadRequest
+				ctx.JSON(resStatusCode, gin.H{
+					"srvResMsg":  http.StatusText(resStatusCode),
+					"srvResData": gin.H{},
+				})
+			}
+		}
+		// 処理で発生したエラーのうちDB関連でないもの
+		var serviceErr *common.CustomErr
+		if errors.As(err, &serviceErr) {
+			// 本処理時のエラーごとに処理(:DBエラー以外)
+			switch serviceErr.Type {
+			case common.ErrTypeGenTokenFailed: // トークンの作成に失敗
+				// エラーログ(トークンの生成に失敗)
+				logging.ErrorLog("Failed to generate token.", err)
+				// レスポンス
+				resStatusCode := http.StatusBadRequest
+				ctx.JSON(resStatusCode, gin.H{
+					"srvResMsg":  http.StatusText(resStatusCode),
+					"srvResData": gin.H{},
+				})
+			default:
+				// エラーログ(新規ユーザー登録が他の問題によりできない)
+				logging.ErrorLog("New user registration was not possible due to other problems.", err)
+				// レスポンス
+				resStatusCode := http.StatusBadRequest
+				ctx.JSON(resStatusCode, gin.H{
+					"srvResMsg":  http.StatusText(resStatusCode),
+					"srvResData": gin.H{},
+				})
+			}
+		}
+		return // エラーレスポンス後に終了
+	}
+
+	// 処理後の成功
+	// 成功ログ
+	logging.SuccessLog("Successful notice registration.")
+	// レスポンス
+	resStatusCode := http.StatusOK
+	ctx.JSON(resStatusCode, gin.H{
+		"srvResMsg": http.StatusText(resStatusCode),
+		"srvResData": gin.H{
+			"authenticationToken": token,
+		},
+	})
+
+}
 
 // お知らせ1件取得
 func GetNoticeDetailHandler(ctx *gin.Context) {
