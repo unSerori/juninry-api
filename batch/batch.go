91ca360a6501e2e4066d55440b2a2f311850d752
MSI\2230294 <2230294@ecc.ac.jp>
2024-07-11 01:27:41 +0000

    [update]バッチ処理の変更と修正

--- batch/batch.go
+++ batch/batch.go
@@ -7,7 +7,11 @@ func DeleteExpiredInviteCodes() {
 	// 期限切れを削除する関数呼びます(クラス招待コード)
 	model.DeleteExpiredInviteCodes()
 	
-	// 期限切れを削除する関数呼びます２(おうち招待コード)
-	model.DeleteExpiredOuchiInviteCodes()
+}
+
+func DeleteExpiredOuchiInviteCodes() {
+	
+		// 期限切れを削除する関数呼びます２(おうち招待コード)
+		model.DeleteExpiredOuchiInviteCodes()
 
 }
