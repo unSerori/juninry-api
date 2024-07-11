f0f1f3f4e2276327e48756727845d01ab5e19067
MSI\2230294 <2230294@ecc.ac.jp>
2024-07-11 00:22:26 +0000

    [add]招待コードの削除関数呼び出し処理

--- batch/batch.go
+++ batch/batch.go
@@ -4,8 +4,10 @@ import "juninry-api/model"
 
 func DeleteExpiredInviteCodes() {
 
-	// 期限切れを削除する関数呼びます
+	// 期限切れを削除する関数呼びます(クラス招待コード)
 	model.DeleteExpiredInviteCodes()
 	
+	// 期限切れを削除する関数呼びます２(おうち招待コード)
+	model.DeleteExpiredOuchiInviteCodes()
 
 }
